8f80514afa46102aa40ee36409679db4ba652951
giga1913 <88190371+giga1913@users.noreply.github.com>
2023-12-09 14:04:49 +0700

    Update SDnu.sh

--- SDnu.sh
+++ SDnu.sh
@@ -25,7 +25,7 @@ CHECKPOINT_MODELS=(
    "https://civitai.com/api/download/models/115086"
    "https://civitai.com/api/download/models/168741"
    "https://civitai.com/api/download/models/178389"
-   "https://civitai.com/api/download/models/142125"
+   "https://civitai.com/api/download/models/238266"
 
 )
 
